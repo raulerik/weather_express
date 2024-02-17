eac3e5e5aa2b29dd0ad770d86a391a185a31c5b1
Raul-Erik Laur <raul.erik.laur@gmail.com>
2024-02-17 04:01:01 +0200

    Added API data to project files

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const fetch = require('node-fetch')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', function (req, res) {
-    res.render('default.ejs')
-})
+const key = '9f188c8987e25d0ea648592653f715c3';
+let city = 'Tartu'
 
+app.get('/', function (req, res) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then((responce) => {
+        return responce.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    })
+})
 app.listen(3000)
